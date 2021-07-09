2bf6ec8a1ea7d075ef9d6d6e9686163c2b9283a6
DCarr40 <davidwcarruthers@yahoo.com>
2021-07-08 21:35:47 -0400

    change app to App w/ jenelle and Golden

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import app from './components/app'
+import App from './components/app'
 
 
-ReactDOM.render(<app/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
