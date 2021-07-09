1edecc9f47dfec0a58f6a12a035f0a85a163a0a1
DCarr40 <davidwcarruthers@yahoo.com>
2021-07-08 22:21:21 -0400

    added axios get for fetchComment w/ jenelle and golden

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class App extends Component {
-    state = { }
+    state = { 
+        commentData: [],
+    }
 
     componentDidMount() {
-
+        // this.fetchVideos();
+        this.fetchComments();
     }
 
-    async fetchVideos() {
-
-    }
+    // async fetchVideos() {
+    //     try {
+    //         let response = await axios.get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=star wars&key=AIzaSyANrDPu3iDPgceB80QmFwv8nkzZcp1w4WU");
+    //      } catch (err) {
+    //        console.log(err);
+    //      }
+    // }
 
     async fetchRelatedVideos() {
 
     }
 
-    async fetchComments() {}
+    async fetchComments() {
+        try {
+            let response = await axios.get("http://localhost:5000/api/collections");
+            this.setState({ commentData: response.data});
+         } catch (err) {
+           console.log(err);
+         }
+    }
 
     render() {
+        console.log(this.state.commentData);
+        if(this.state.commentData[0] ===  undefined){
+            console.log(this.state.commentData);
+            return (
+            <div>Loading...</div>
+            )
+        }
         return(
-            <h1>Hello World</h1>
+            <React.Fragment>
+                <div>{this.state.commentData.entries}</div>
+            </React.Fragment>
         )
+
     }
 }
