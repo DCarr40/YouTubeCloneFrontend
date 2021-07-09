db38728b896f4a9ffd90ec094039b6f5ff724ceb
DCarr40 <davidwcarruthers@yahoo.com>
2021-07-08 21:44:33 -0400

    added fetch functs to App w/ jenelle and Golden

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
     state = { }
+
+    componentDidMount() {
+
+    }
+
+    async fetchVideos() {
+
+    }
+
+    async fetchRelatedVideos() {
+
+    }
+
+    async fetchComments() {}
+
     render() {
         return(
             <h1>Hello World</h1>
